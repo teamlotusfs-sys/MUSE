5692f7b8771355884ea0b3f18d8594bdb9f8e421
teamlotusfs-sys <teamlotusfs@gmail.com>
2026-04-16 22:22:54 -0400

    Change model from mixtral to llama for playlist generation

--- pages/api/generate-playlist.js
+++ pages/api/generate-playlist.js
@@ -63,7 +63,7 @@ export default async function handler(req, res) {
     });
 
     const message = await client.chat.completions.create({
-      model: "mixtral-8x7b-32768",
+      model: "llama-3.1-8b-instant",
       max_tokens: 1024,
       messages: [
         {
